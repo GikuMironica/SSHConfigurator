949115ef9785b5934de9c8e8619278413a71eb28
mironicagicu <mironicagicu@gmail.com>
2020-10-04 21:12:24 +0200

    [logging] Logging Fixed

--- SSHConfigurator/Program.cs
+++ SSHConfigurator/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
 using NLog.Web;
 
 namespace SSHConfigurator
@@ -22,7 +24,24 @@ namespace SSHConfigurator
         /// <param name="args"></param>
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var config = new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                .AddJsonFile("NLog.json", optional: true, reloadOnChange: true).Build();
+            LogManager.Configuration = new NLogLoggingConfiguration(config.GetSection("NLog"));
+
+            var logger = NLogBuilder.ConfigureNLog(LogManager.Configuration).GetCurrentClassLogger();
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Shut down program because of error");
+            }
+            finally
+            {
+                LogManager.Shutdown();
+            }
         }
 
 
@@ -35,7 +54,16 @@ namespace SSHConfigurator
                 {
                     config.AddJsonFile(Path.Combine(Environment.CurrentDirectory, "wwwroot", "scripts", "linux", "BashScripts.json"), optional: false, reloadOnChange: true);
                     config.AddJsonFile(Path.Combine(Environment.CurrentDirectory, "wwwroot", "scripts", "windows", "PowerShellScripts.json"), optional: false, reloadOnChange: true);
-                })                
+                })
+                 .ConfigureLogging((hostingContext, logging) =>
+                 {
+                     logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                     logging.AddConsole();
+                     logging.AddDebug();
+                     logging.AddEventSourceLogger();
+                     // Enable NLog as one of the Logging Provider
+                     logging.AddNLog();
+                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
